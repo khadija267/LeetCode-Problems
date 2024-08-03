b5bb132cf39761581d4199b3236dcd56d179913f
Khadija Hesham <37675645+khadija267@users.noreply.github.com>
2024-08-03 15:28:48 +0300

    Time: 167 ms (5.08%), Space: 17 MB (19.82%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,17 +1,33 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        count = 0
-        size = len(flowerbed)
-        i = 0
-
-        while i < size:
-            if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == size - 1 or flowerbed[i + 1] == 0):
-                count += 1
-                i += 2  # Skip the next plot since we cannot plant a flower there
-            else:
-                i += 1
-
-            if count >= n:
+        print('n before',n)
+        len_flowerbed=len(flowerbed)-1
+        for index, item in enumerate(flowerbed):
+            if n==0:
                 return True
-
-        return False
+            if len_flowerbed==0:
+                if flowerbed[index]==1:
+                    return False
+                else:
+                    return True
+                break
+            
+            if ((flowerbed[0]==0)&(flowerbed[1]==0)):
+                flowerbed[0]=1
+                # print('new flowerbed',flowerbed)
+                n-=1
+            if ((flowerbed[len_flowerbed]==0)&(flowerbed[len_flowerbed-1]==0)) :
+                flowerbed[len_flowerbed]=1
+                # print('new flowerbed',flowerbed)
+                n-=1
+            if (index!=0)&(index!=(len_flowerbed)):
+                if((flowerbed[index-1]) == 0) & ((flowerbed[index+1]) == 0):
+                    if flowerbed[index]!=1:
+                        flowerbed[index]=1
+                        # print('new flowerbed',flowerbed)
+                        n-=1
+        print('n after',n)
+        if n<=0:
+            return True
+        else:
+            return False
