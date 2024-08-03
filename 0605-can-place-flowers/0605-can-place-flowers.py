78524a2aa854b418670467ffe6d8fe81aab90660
Khadija Hesham <37675645+khadija267@users.noreply.github.com>
2024-08-03 15:29:54 +0300

    Time: 150 ms (6.25%), Space: 17 MB (19.82%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,6 +1,5 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        print('n before',n)
         len_flowerbed=len(flowerbed)-1
         for index, item in enumerate(flowerbed):
             if n==0:
@@ -11,22 +10,17 @@ class Solution:
                 else:
                     return True
                 break
-            
             if ((flowerbed[0]==0)&(flowerbed[1]==0)):
                 flowerbed[0]=1
-                # print('new flowerbed',flowerbed)
                 n-=1
             if ((flowerbed[len_flowerbed]==0)&(flowerbed[len_flowerbed-1]==0)) :
                 flowerbed[len_flowerbed]=1
-                # print('new flowerbed',flowerbed)
                 n-=1
             if (index!=0)&(index!=(len_flowerbed)):
                 if((flowerbed[index-1]) == 0) & ((flowerbed[index+1]) == 0):
                     if flowerbed[index]!=1:
                         flowerbed[index]=1
-                        # print('new flowerbed',flowerbed)
                         n-=1
-        print('n after',n)
         if n<=0:
             return True
         else:
