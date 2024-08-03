ad6f257133f5d238426d310f372f96cef2949039
Khadija Hesham <37675645+khadija267@users.noreply.github.com>
2024-08-03 13:13:51 +0300

    Time: 161 ms (5.55%), Space: 28.4 MB (24.42%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
@@ -3,8 +3,7 @@ class Solution:
         import numpy as np
         result=[]
         max_candies=max(candies)
-        # new_candies_list= [i+extraCandies for i in candies]
-        new_candies_list=np.array(candies)+extraCandies
+        new_candies_list= [i+extraCandies for i in candies]
         for i in new_candies_list:
             if i>=max_candies:
                 result.append(True)
