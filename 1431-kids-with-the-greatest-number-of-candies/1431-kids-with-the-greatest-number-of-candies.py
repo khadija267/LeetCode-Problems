6c69462e7389126033cf4d2776d185381e7e0ef8
Khadija Hesham <37675645+khadija267@users.noreply.github.com>
2024-10-20 14:40:42 +0300

    Delete 1431-kids-with-the-greatest-number-of-candies directory

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        res=[False] * len(candies)
-        max_candies= max(candies)
-        for ind,item in enumerate(candies):
-            if item+extraCandies>= max_candies:
-                res[ind]=True
-        return res
