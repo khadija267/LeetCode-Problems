fbe8dc93049d25d7215a4b4dda7828814f803f29
Khadija Hesham <37675645+khadija267@users.noreply.github.com>
2024-11-30 17:24:42 +0200

    Time: 31 ms (14.57%), Space: 17.7 MB (7.36%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -2,28 +2,23 @@ class Solution:
     def compress(self, chars: List[str]) -> int:
         if len(chars)==1:
             return 1
-        chars_copy=chars
-        import random
-        last_elm=chars[-1]
-        # random_char=random.choice([i for i in chars if i != last_elm])
-        chars_copy.append("X")
+        chars.append("X")
         s=""
         count=1
         len_chars=len(chars)-1
-        chars_copy=chars
         for i in range(len_chars):
             
             print('first in loop char ',chars[i])
             print('first in loop count ',count)
 
-            if chars_copy[i]==chars_copy[i+1]:
+            if chars[i]==chars[i+1]:
                 print('current = further increment count')
                 print('count before ', count)
                 count=count+1
                 print('count after ', count)
 
             else:
-                s=s+chars_copy[i]
+                s=s+chars[i]
                 print('current != further increment count, reset count')
                 if count>1:
                     print('count greater than 1, append')
